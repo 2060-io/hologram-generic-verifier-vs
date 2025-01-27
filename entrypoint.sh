3e4d8ba5d3e2679e2af41e0890913487ed074147
Andres Vallecilla <andresfelipe083195@hotmail.com>
2025-01-26 19:21:11 -0500

    fix:: add docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_PORT#$NEXT_PUBLIC_PORT#g"
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_BASE_URL#$NEXT_PUBLIC_BASE_URL#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_CREDENTIAL_DEFINITION_ID#$CREDENTIAL_DEFINITION_ID#g"
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_SERVICE_AGENT_ADMIN_BASE_URL#$SERVICE_AGENT_ADMIN_BASE_URL#g"
 
 echo "Starting Nextjs"
--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
     CREDENTIAL_DEFINITION_ID: process.env.CREDENTIAL_DEFINITION_ID || 'did:web:chatbot-demo.dev.2060.io?service=anoncreds&relativeRef=/credDef/HngJhYMeTLTZNa5nJxDybmXDsV8J7G1fz2JFSs3jcouT',
     SERVICE_AGENT_ADMIN_BASE_URL: process.env.SERVICE_AGENT_ADMIN_BASE_URL || 'https://a.chatbot-demo.dev.2060.io',
+    NEXT_TELEMETRY_DISABLED: '1',
   }
 };
 
