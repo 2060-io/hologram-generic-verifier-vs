43f8ce6abf555da53e07c74f43f2dbb1f7db33c3
Andres Vallecilla <andresfelipe083195@hotmail.com>
2025-01-27 16:39:11 -0500

    Merge branch 'main' into feat/create-dockerfile-and-docker-compose-for-local-deployment

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_PORT: process.env.NEXT_PUBLIC_PORT || '3000',
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
-    CREDENTIAL_DEFINITION_ID: process.env.CREDENTIAL_DEFINITION_ID || 'did:web:chatbot-demo.dev.2060.io?service=anoncreds&relativeRef=/credDef/HngJhYMeTLTZNa5nJxDybmXDsV8J7G1fz2JFSs3jcouT',
-    SERVICE_AGENT_ADMIN_BASE_URL: process.env.SERVICE_AGENT_ADMIN_BASE_URL || 'https://a.chatbot-demo.dev.2060.io',
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || `http://localhost:${process.env.NEXT_PUBLIC_PORT || '3000'}`,
     NEXT_TELEMETRY_DISABLED: '1',
   }
 };
